410dd4f1381d92c8d287bb71df9818276d108446
Reinhardt <notmarx.tech@gmail.com>
2021-10-18 20:00:01 +0700

    Add Interaction

--- Source/Detriot.ts
+++ Source/Detriot.ts
@@ -1,9 +1,18 @@
 /* [WIP] This entire file will be rework */
 import { ShardClient } from "detritus-client";
+import { ApplicationCommandTypes, InteractionCallbackTypes, MessageFlags } from "detritus-client/lib/constants";
+import { InteractionDataApplicationCommand } from "detritus-client/lib/structures";
 import { GatewayIntents } from "detritus-client-socket/lib/constants";
 import { TOKEN, PREFIX } from "../config.json";
 
 const client = new ShardClient(TOKEN, { cache: true, gateway: { intents: [GatewayIntents.GUILDS, GatewayIntents.GUILD_MESSAGES] } });
+
+client.on("gatewayReady", () => {
+    console.log(`${client.user.tag} is now Online!`);
+    client.gateway.setPresence({ activity: { name: "Detriot", type: 1 }, status: "idle" });
+
+    
+});
 
 client.on("messageCreate", async ({ message }) => {
     if (message.author.bot) return;
@@ -16,7 +25,21 @@ client.on("messageCreate", async ({ message }) => {
     }
 });
 
-client.run().then(() => {
-    console.log(`${client.user.tag} is now Online!`);
-    client.gateway.setPresence({ activity: { name: "Detriot", type: 1 }, status: "idle" });
+client.on("interactionCreate", async ({ interaction }) => {
+    if (!interaction.guild || interaction.member.bot) return;
+
+    const data = interaction.data as InteractionDataApplicationCommand;
+
+    if (data.type === ApplicationCommandTypes.CHAT_INPUT) {
+        switch (data.name) {
+            case "ping":
+                interaction.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, {
+                    content: "Pong!",
+                    flags: MessageFlags.EPHEMERAL
+                });
+                break;
+        }
+    }
 });
+
+client.run();
